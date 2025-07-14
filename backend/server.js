15d24e2f50c28a0577b91a994f2a306466f937b9
Aadi Tyagi <aadityagi1232@gmail.com>
2025-07-14 23:30:22 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,44 +1,44 @@
-//from .env file
 require("dotenv").config();
-
 
 const express = require("express");
 const cors = require("cors");
 const http = require("http");
 const { Server } = require("socket.io");
+
 const connectDB = require("./config/db");
 const noteRoutes = require("./routes/noteRoutes");
 const noteSocket = require("./sockets/noteSocket");
 
-
 const app = express();
-
-
 const server = http.createServer(app);
 
-// setup Socket.IO on the server
+// setup Socket.IO
 const io = new Server(server, {
   cors: {
     origin: "*", // allow all
   },
 });
 
-
+// connect to MongoDB
 connectDB();
 
-// middleware to allow frontend access and parse json
+// middleware
 app.use(cors());
 app.use(express.json());
 
-// route for handling notes
+
+app.get("/", (req, res) => {
+  res.send("Backend is activated and running!");
+});
+
+// note routes
 app.use("/notes", noteRoutes);
 
-// handle real-time socket connections
+// socket setup
 noteSocket(io);
 
 
 const PORT = process.env.PORT || 5000;
-
 server.listen(PORT, () => {
   console.log("Server is running at http://localhost:" + PORT);
 });
